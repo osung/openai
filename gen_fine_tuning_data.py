fbe9c38ccfb2d7d77e40f688e3d2a7ccc296384d
Joong-Youn Lee <osungs@gmail.com>
2023-10-13 07:33:13 +0900

    disabled certification and change the gpt model from 3.5 to 4 for the pdf code

--- gen_fine_tuning_data.py
+++ gen_fine_tuning_data.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ['CURL_CA_BUNDLE'] = 'C:\work\kisti_cert.crt'
+#os.environ['CURL_CA_BUNDLE'] = 'C:\work\kisti_cert.crt'
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-question = "generate ten pairs of question and answers from following text and give me answers as following structure :{\"prompt\": question, \"completion\": answer}: "
+question = "generate five pairs of question and answers from following text and give me answers as following structure :{\"prompt\": question, \"completion\": answer}: "
 
-ko_context = """양자 암호화 상업화는 고객 인식 부족으로 어려움이 있으며, 지식은 경제 성장의 주요 동력 중 하나입니다. 
+kr_context = """양자 암호화 상업화는 고객 인식 부족으로 어려움이 있으며, 지식은 경제 성장의 주요 동력 중 하나입니다. 
 고기술 혁신을 상업화하기 위해서는 기업 상업화 팀이 필요한 기술 지식을 식별, 확보, 결합 및 관리해야 합니다. 
 또한 혁신이 성공하려면 혁신 팀이 직원의 학습 경로를 준수하고 강력한 네트워크를 생성 및 유지할 수 있어야 합니다. 
 양자 암호화가 새로운 개념이기 때문에 충분하고 관련성 있는 시장 정보를 얻지 못하거나 정보를 올바르게 활용하지 못하는 등 다양한 어려움이 나타나고 있으며, 
@@ -27,7 +27,7 @@ Therefore, proper customer awareness of quantum cryptography solutions is needed
 These are some of the major challenging factors that are affecting the market growth.'''
 
 messages = [
-    {"role": "system", "content": "You are a industrial market expert. You have to give specialied and detailed answers. Always anaswer in Korean."},
+    {"role": "system", "content": "You are an industrial market expert. You have to give specialied and detailed answers.Always anaswer in Korean."},
     {"role": "user", "content": question + en_context }
 ]
 
